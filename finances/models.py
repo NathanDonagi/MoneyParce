8126fb81c0fee2c9cff3dbb4e626f46ee8448ddb
Donagi, Nathan Cyrus <ndonagi3@gatech.edu>
2025-04-20 19:45:29 -0400

    working on website with Kai

--- finances/models.py
+++ finances/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,7 +7,11 @@ from accounts.models import CustomUser
 
 
 class Budget(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     name = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
     limit = models.IntegerField()
@@ -13,7 +19,11 @@ class Budget(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='budgets')
 
 class Transaction(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     name = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
@@ -23,4 +33,3 @@ class Transaction(models.Model):
 
     def __str__(self):
         return str(self.id) + ' - ' + self.description
-
--- finances/urls.py
+++ finances/urls.py
@@ -3,7 +3,10 @@ from . import views
 urlpatterns = [
     path('profile/', views.profile, name='finances.profile'),
     path('budgets/', views.budgets, name='finances.budgets'),
+    path('budgets/form', views.budgets_form, name='finances.budgets_form'),
     path('transactions/', views.transactions, name='finances.transactions'),
+    path('transactions/form', views.transactions_form, name='finances.transactions_form'),
+    path('transactions/delete/<uuid:transaction_id>/', views.delete_transaction, name='finances.delete_transaction'),
+    path('transactions/edit/<uuid:transaction_id>/', views.edit_transaction, name='finances.edit_transaction'),
     path('progress/', views.progress, name='finances.progress'),
-    path('advice/', views.advice, name='finances.advice'),
 ]
