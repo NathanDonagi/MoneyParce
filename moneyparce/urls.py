84b4af30c224c3c8750311aad8719d86ec0d3cfd
Philip Dobranowski <dobranowskip1@gmail.com>
2025-04-23 17:48:36 -0400

    Added reports page functionality and updated gitignore to remove garbage files

--- moneyparce/urls.py
+++ moneyparce/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('finances/', include('finances.urls')),
     path('accounts/', include('accounts.urls')),
+    path('reports/', include('reports.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
